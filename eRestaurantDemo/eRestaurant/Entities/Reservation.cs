834c67369fb53bb5a306faceb1ec8e6cc259c22b
Clayton Radke <claytonradke@gmail.com>
2016-09-22 16:54:28 -0600

    Added new entities

---
+++ eRestaurantDemo/eRestaurant/Entities/Reservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace eRestaurant.Entities
+{
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public int NumberInParty { get; set; }
+        public string ContactPhone { get; set; }
+        public string ReservationStatus { get; set; }
+        public string EventCode { get; set; }
+
+        public virtual SpecialEvent Event { get; set; }
+    }
+}
---
+++ eRestaurantDemo/eRestaurant/Entities/SpecialEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace eRestaurant.Entities
+{
+    public class SpecialEvent
+    {
+        [Key]
+        public string EventCode { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
