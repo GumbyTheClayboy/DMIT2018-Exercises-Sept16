c6ba2f15dd2b9a9af72aebd2b94425b881f49d45
Clayton Radke <claytonradke@gmail.com>
2016-09-27 16:19:23 -0600

    Created BLL MenuController

---
+++ eRestaurantDemo/eRestaurant/BLL/MenuController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eRestaurant.DAL;
+using eRestaurant.Entities.DTOs;
+using eRestaurant.Entities.POCOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace eRestaurant.BLL
+{
+    public class MenuController
+    {
+        public List<CategoryWithItems> GetRestaurantMenu()
+        {
+            using (var context = new RestaurantContext())
+            {
+                var data = from aCategory in context.MenuCategories
+                           orderby aCategory.Description
+                           select new CategoryWithItems()
+                           {
+                               Description = aCategory.Description,
+                               MenuItems = (from item in aCategory.MenuItems
+                                            orderby item.Description
+                                            select new MenuItem()
+                                            {
+                                                ItemDescription = item.Description,
+                                                Price = item.CurrentPrice,
+                                                Calories = item.Calories,
+                                                Comment = item.Comment
+                                            }).ToList()
+                           };
+                return data.ToList();
+            }
+        }
+    }
+}
---
+++ eRestaurantDemo/eRestaurant/Entities/DTOs/CategoryWithItems.cs
@@ -0,0 +1,15 @@
+﻿using eRestaurant.Entities.POCOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities.DTOs
+{
+    public class CategoryWithItems
+    {
+        public string Description { get; set; }
+        public IEnumerable<MenuItem> MenuItems { get; set; }
+    }
+}
